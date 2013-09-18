5fece37ac9a17b0e65b3aa34a452d111dd1f4f05
tm <tomasz.mielniczek@elavon.com>
2013-09-18 17:16:19 +0200

    User class added.

---
+++ Typer.Domain/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Typer.Domain.Entities
+{
+    public class User
+    {
+
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        private string password;
+
+    }
+}
