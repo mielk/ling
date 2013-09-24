ceb9961d0aa6abb731a41e3cceabf96ce4732d9e
txmieln <tomasz.mielniczek@elavon.com>
2013-09-24 12:35:11 +0200

    Two implementations of IUsersRepository added (UsersRepository and EFUsersRepository).

---
+++ Typer.Domain/Concrete/EFUsersRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Typer.Domain.Abstract;
+using Typer.Domain.Entities;
+
+namespace Typer.Domain.Concrete
+{
+    public class EFUsersRepository : IUsersRepository
+    {
+
+        private static IUsersRepository instance;
+
+        private EFUsersRepository()
+        {
+            
+        }
+
+        public static IUsersRepository getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new EFUsersRepository();
+            }
+            return instance;
+        }
+
+
+        //------------
+        
+
+        public User getUser(int userID)
+        {
+            return null;
+        }
+
+        public User getUser(string username, string password)
+        {
+            return null;
+        }
+
+    }
+}
---
+++ Typer.Domain/Concrete/UsersRepositoryFactory.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Typer.Domain.Abstract;
+using Typer.Domain.Concrete;
+
+namespace Typer.Domain.Entities
+{
+    public class UsersRepositoryFactory
+    {
+
+        private static UsersRepositoryFactory instance;
+        public IUsersRepository Repository { get; private set; }
+
+
+
+
+
+        private UsersRepositoryFactory(IUsersRepository repository)
+        {
+            Repository = repository;
+        }
+
+
+        public static UsersRepositoryFactory getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new UsersRepositoryFactory(getRepoInstance());
+            }
+
+            return instance;
+
+        }
+
+
+        private static IUsersRepository getRepoInstance()
+        {
+            return EFUsersRepository.getInstance();
+        }
+
+
+
+
+    }
+}
