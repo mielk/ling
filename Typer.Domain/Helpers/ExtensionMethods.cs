30c3d548bd0c63aa6e1d74f426c6b471c869d22c
txmieln <tomasz.mielniczek@elavon.com>
2013-09-24 10:52:34 +0200

    Extension method added to check if the given string is null or empty.

--- Typer.Domain/Helpers/ExtensionMethods.cs
+++ Typer.Domain/Helpers/ExtensionMethods.cs
@@ -6,9 +6,13 @@ using System.Threading.Tasks;
 
 namespace Typer.Domain.Helpers
 {
-    public class ExtensionMethods
+    public static class ExtensionMethods
     {
 
+        public static bool isNullOrEmpty(this string text)
+        {
+            return (text == null || text.Length == 0 ? true : false);
+        }
 
     }
 }
--- Typer.Tests.UnitTests/Helpers/ExtensionMethodsUnitTests.cs
+++ Typer.Tests.UnitTests/Helpers/ExtensionMethodsUnitTests.cs
@@ -1,14 +1,36 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Typer.Domain.Helpers;
 
 namespace Typer.Tests.UnitTests.Helpers
 {
     [TestClass]
     public class ExtensionMethodsUnitTests
     {
+
         [TestMethod]
-        public void TestMethod1()
+        public void IsNullOrEmpty_returns_true_for_null_string()
         {
+            String s = null;
+            Assert.IsTrue(s.isNullOrEmpty());
         }
+
+
+        [TestMethod]
+        public void IsNullOrEmpty_returns_true_for_empty_string()
+        {
+            String s = string.Empty;
+            Assert.IsTrue(s.isNullOrEmpty());
+        }
+
+
+        [TestMethod]
+        public void IsNullOrEmpty_returns_false_for_non_empty_string()
+        {
+            String s = "a";
+            Assert.IsFalse(s.isNullOrEmpty());
+        }
+
+
     }
 }
