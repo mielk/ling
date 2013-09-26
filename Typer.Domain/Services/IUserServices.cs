d54d222709c80e66ad542a39f869eb176f36bb58
txmieln <tomasz.mielniczek@elavon.com>
2013-09-26 13:32:01 +0200

    Interface IUserServices added and class UserServices that implements this interface.

---
+++ Typer.Domain/Services/IUserServices.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Typer.Domain.Services
+{
+    public class IUserServices
+    {
+        bool Authentication(string username, string password);
+    }
+}
---
+++ Typer.Domain/Services/UserServices.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Typer.Domain.Services
+{
+    public class UserServices : IUserServices
+    {
+
+        public bool Authentication(string username, string password)
+        {
+            throw new Exception("Not implemented yet.");
+        }
+
+    }
+}
