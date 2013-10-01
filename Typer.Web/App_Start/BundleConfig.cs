caca21b9b128144e9f8c3e059772ddc436a70a68
mielk <mielk@o2.pl>
2013-10-01 17:04:30 +0200

    JavaScript code added to client validation when creating account.

--- Typer.Web/App_Start/BundleConfig.cs
+++ Typer.Web/App_Start/BundleConfig.cs
@@ -22,8 +22,8 @@ namespace Typer.Web
                                     "~/Scripts/app/login.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/registration").Include(
-                                    "~/Scripts/app/register.js", 
-                                    "~/Scripts/app/util.js"));
+                                    "~/Scripts/app/util.js",
+                                    "~/Scripts/app/register.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
--- Typer.Web/Scripts/app/register.js
+++ Typer.Web/Scripts/app/register.js
@@ -1,46 +1,136 @@
 ﻿$(document).ready(function () {
     adjustPlaceholder();
+    model();
 });
 
 function adjustPlaceholder() {
     var supported = Modernizr.input.placeholder;
-    $("#account label").css({ 'display': (supported ? 'none' : 'block') });
+    $("#account label").css({ 'display': 'block' });
 }
 
 
-var userRegistrator = (function () {
+
+
+var model = function (){
 
     //Confirmation button.
-    var registerButton = $("btnRegister")[0];
+    var btn = $("#btnRegister")[0];
 
-    //Properties.
-    var username = "";
-    var password = "";
-    var confirmPassword = "";
-    var email = "";
-    var country = "";
+    //Inactive controls.
+    var inactive = new HashTable(null);
 
-    //HTML fields.
-    var controls = {
-        username: $("Username")[0],
-        password: $("Password")[0],
-        confirmPassword: $("ConfirmPassword")[0],
-        email: $("Email")[0],
-        country: $("Country")[0]
-    }
+    //Control groups.
+    var controls = new HashTable(null);
+    controls.setItem('username', new ControlGroup('div_username', checkUsername));
+    controls.setItem('password', new ControlGroup('div_password', checkPassword));
     
 
-    (function applyRequiredStyle() {
-        alert(100);
-    })();
+
+    
+
+    function isValid() {
+        return (inactive.size() > 0 ? false : true);
+    }
+
+    function checkState() {
+        alert("Not implemented yet.");
+    }
+
+
 
 
     return {
 
-        setUsername: function (username) {
+        getControlGroup: function (name) {
+            return controls.getItem(name);
+        },
 
+        addToInactive: function (control) {
+            inactive.setItem(control, true);
+        },
+
+        removeFromInactive: function (control) {
+            inactive.removeItem(control);
         }
 
     }
 
-})();
+
+
+};
+
+
+
+
+function ControlGroup(id, fn) {
+    var me = this; //To be used in events binding.
+
+    this.container = $('#' + id)[0];
+
+    this.getControl = function(selector){
+        return $(this.container).find('.' + selector)[0];
+    }
+
+    this.formatAsValid = function () {
+        alert('valid');
+    }
+
+    this.formatAsInvalid = function () {
+        alert('invalid');
+    }
+
+    //Bind change event to value control.
+    $(this.getValueControl()).bind({
+        'change': function () {
+            var isValid = fn(me.getValue());
+            me.format(isValid);
+        }
+    });
+
+}
+ControlGroup.prototype.getContainer = function () {
+    return this.container;
+}
+ControlGroup.prototype.getValueControl = function () {
+    return this.getControl('default');
+}
+ControlGroup.prototype.getStateIconControl = function () {
+    return this.getControl('icon');
+}
+ControlGroup.prototype.getErrorControl = function () {
+    return this.getControl('error');
+}
+ControlGroup.prototype.getValue = function () {
+    var ctrl = $(this.getValueControl());
+    return ctrl.val();
+}
+ControlGroup.prototype.format = function (isValid) {
+    if (isValid) {
+        this.formatAsValid();
+    } else {
+        this.formatAsInvalid();
+    }
+}
+
+
+
+
+
+
+function checkUsername(username) {
+    alert('checkUsername for ' + username);
+    return true;
+}
+
+function checkPassword(password) {
+    alert('checkPassword for ' + password);
+    return false;
+}
+
+function checkIfPasswordsMatch(password, confirmPassword) {
+
+}
+
+function checkMail(mail) {
+
+}
