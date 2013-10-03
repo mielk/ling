a3b8cf7335cfab56435c631151c5bddd0695af79
txmieln <tomasz.mielniczek@elavon.com>
2013-10-03 13:56:45 +0200

    Server-side validation added for user registration process.

--- Typer.BLL/Services/Concrete/UserRegistrationData.cs
+++ Typer.BLL/Services/Concrete/UserRegistrationData.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+using Typer.Domain.Helpers;
 
 namespace Typer.BLL.Services
 {
     public class UserRegistrationData
     {
+
+        public static readonly int UserNameMinimumLength = 5;
+        public static readonly int UserNameMaximumLength = 20;
+        public static readonly int PasswordMinimumLength = 6;
+
 
         [Required]
         [Display(Name = "User name", Prompt = "Enter user name")]
@@ -42,6 +49,64 @@ namespace Typer.BLL.Services
         //[Required]
         //[Display(Name = "Date of birth")]
         public DateTime DateOfBirth { get; set; }
+
+
+
+        public bool isValid()
+        {
+            if (!isUserNameValid())
+                return false;
+
+            if (!isPasswordValid())
+                return false;
+
+            if (!arePasswordsMatching())
+                return false;
+
+            if (!isMailValid())
+                return false;
+
+            return true;
+
+        }
+
+
+        private bool isUserNameValid()
+        {
+            if (UserName == null || UserName.Length < UserNameMinimumLength || UserName.Length > UserNameMaximumLength)
+                return false;
+
+            return true;
+
+        }
+
+        private bool isPasswordValid()
+        {
+
+            string regex = @"^.*(?=.*\d)(?=.*[a-zA-Z]).*$";
+
+            if (Password == null || Password.Length < PasswordMinimumLength)
+                return false;
+
+            return Regex.IsMatch(Password, regex);
+
+        }
+
+        private bool arePasswordsMatching()
+        {
+
+            if (ConfirmPassword == null || ConfirmPassword == string.Empty)
+                return false;
+
+            return (ConfirmPassword == Password);
+
+        }
+
+        private bool isMailValid()
+        {
+            return Email.isLegalMail();
+        }
+
 
     }
 }
--- Typer.Tests.UnitTests/Domain/UserRegistrationDataTests.cs
+++ Typer.Tests.UnitTests/Domain/UserRegistrationDataTests.cs
@@ -1,14 +1,141 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Typer.BLL.Services;
 
 namespace Typer.Tests.UnitTests.Domain
 {
     [TestClass]
     public class UserRegistrationDataTests
     {
+
+        private string username = "username";
+        private string password = "haslo1";
+        private string confirmPassword = "haslo1";
+        private string email = "mail@mail.com";
+
+
         [TestMethod]
-        public void TestMethod1()
+        public void for_proper_data_set_object_is_valid()
         {
+            UserRegistrationData urd = getValidDataSet();
+            Assert.IsTrue(urd.isValid());
         }
+
+
+        [TestMethod]
+        public void if_username_is_too_short_data_are_invalid()
+        {
+            UserRegistrationData urd = getValidDataSet();
+            urd.UserName = new string('a', UserRegistrationData.UserNameMinimumLength - 1);
+            Assert.IsFalse(urd.isValid());
+        }
+
+
+        [TestMethod]
+        public void if_username_is_too_long_data_are_invalid()
+        {
+            UserRegistrationData urd = getValidDataSet();
+            urd.UserName = new string('a', UserRegistrationData.UserNameMaximumLength + 1);
+            Assert.IsFalse(urd.isValid());
+        }
+
+
+        [TestMethod]
+        public void if_username_is_empty_or_null_data_are_invalid()
+        {
+            UserRegistrationData urd = getValidDataSet();
+            urd.UserName = "";
+            Assert.IsFalse(urd.isValid());
+
+            urd.UserName = null;
+            Assert.IsFalse(urd.isValid());
+
+        }
+
+
+        [TestMethod]
+        public void if_password_is_empty_or_null_data_are_invalid()
+        {
+            UserRegistrationData urd = getValidDataSet();
+            urd.Password = "";
+            Assert.IsFalse(urd.isValid());
+
+            urd.Password = null;
+            Assert.IsFalse(urd.isValid());
+
+        }
+
+
+        [TestMethod]
+        public void if_password_is_too_short_data_are_invalid()
+        {
+            UserRegistrationData urd = getValidDataSet();
+            urd.Password = new string('a', UserRegistrationData.PasswordMinimumLength - 1);
+            Assert.IsFalse(urd.isValid());
+        }
+
+
+        [TestMethod]
+        public void if_password_contains_no_letter_data_are_invalid()
+        {
+            UserRegistrationData urd = getValidDataSet();
+            urd.Password = "12345678_0";
+            Assert.IsFalse(urd.isValid());
+        }
+
+        [TestMethod]
+        public void if_password_contain_no_number_data_are_invalid()
+        {
+            UserRegistrationData urd = getValidDataSet();
+            urd.Password = "abcdefghijkl_";
+            Assert.IsFalse(urd.isValid());
+        }
+
+
+        [TestMethod]
+        public void if_confirm_password_is_empty_or_null_data_are_invalid()
+        {
+            UserRegistrationData urd = getValidDataSet();
+            urd.ConfirmPassword = "";
+            Assert.IsFalse(urd.isValid());
+
+            urd.ConfirmPassword = null;
+            Assert.IsFalse(urd.isValid());
+
+        }
+
+
+        [TestMethod]
+        public void if_passwords_doesnt_match_data_are_invalid()
+        {
+            UserRegistrationData urd = getValidDataSet();
+            urd.Password = "H@sl01";
+            urd.ConfirmPassword = "H@sl02";
+            Assert.IsFalse(urd.isValid());
+        }
+
+
+        [TestMethod]
+        public void if_mail_is_illegal_data_are_invalid()
+        {
+            UserRegistrationData urd = getValidDataSet();
+            string[] illegalMails = { "mail.o2.pl", "mail@o2", "@o2.pl", "mail mail@o2.pl", "mail@o2.pl.", "mail@o2..pl" };
+
+            foreach (string mail in illegalMails)
+            {
+                urd.Email = mail;
+                Assert.IsFalse(urd.isValid());
+            }
+
+        }
+
+
+
+        private UserRegistrationData getValidDataSet()
+        {
+            return new UserRegistrationData { UserName = username, Password = password, ConfirmPassword = confirmPassword, Email = email };
+        }
+
+
     }
 }
