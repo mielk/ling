3ff93681587060420c46801861372288aa3b06bb
txmieln <tomasz.mielniczek@elavon.com>
2013-10-17 17:12:05 +0200

    [Added] ID and Name fields added to [Edit] view.

--- Typer.Domain/Entities/Question.cs
+++ Typer.Domain/Entities/Question.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Typer.Domain.Entities
 {
@@ -12,15 +13,34 @@ namespace Typer.Domain.Entities
         public const int MinWeight = 1;
         public const int MaxWeight = 10;
 
+        [Display(Name = "Id")]
         public int Id { get; set; }
+
+        [Display(Name = "Name")]
         public string Name { get; set; }
+
+        [Display(Name = "Weight")]
         public int Weight { get; set; }
+
+        [Display(Name = "Is active")]
         public bool IsActive { get; set; }
+
+        [Display(Name = "Creator")]
         public int CreatorId { get; set; }
+
+        [Display(Name = "Creating date")]
         public DateTime CreateDate { get; set; }
+
+        [Display(Name = "Is approved")]
         public bool IsApproved { get; set; }
+
+        [Display(Name = "Positive votes")]
         public int Positive { get; set; }
+
+        [Display(Name = "Negative votes")]
         public int Negative { get; set; }
+
+        [Display(Name = "Is complex")]
         public bool IsComplex { get; set; }
 
     }
