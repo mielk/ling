fffa8fc291824e487f9930cc9794d6b37f628a70
txmieln <tomasz.mielniczek@elavon.com>
2013-10-18 15:57:43 +0200

    UserLanguages table redefined.

--- Typer.DAL/TransferObjects/LanguageUserDto.cs
+++ Typer.DAL/TransferObjects/LanguageUserDto.cs
@@ -8,6 +8,7 @@ namespace Typer.DAL.TransferObjects
 {
     public class LanguageUserDto
     {
+        public int Id { get; set; }
         public int LanguageId { get; set; }
         public int UserId { get; set; }
     }
