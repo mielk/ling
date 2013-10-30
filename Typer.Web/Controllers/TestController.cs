1a4d9ddeaa54b9bc99940ecc3a6ed8d9acc0a1bc
mielk <mielk@o2.pl>
2013-10-30 16:31:29 +0100

    Wybieranie opcji dla pytania.

--- Typer.Web/Controllers/TestController.cs
+++ Typer.Web/Controllers/TestController.cs
@@ -3,16 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Typer.Domain.Services;
+using Typer.DAL.Repositories;
+using Typer.Domain.Entities;
+using Typer.Web.Models;
 
 namespace Typer.Web.Controllers
 {
     public class TestController : Controller
     {
 
+        private IQuestionService questionService;
+        private ILanguageService languageService;
+
+        public TestController(IQuestionService questionService, ILanguageService languageService)
+        {
+            this.questionService = questionService;
+            this.languageService = languageService;
+        }
+
+
         [AllowAnonymous]
         public ActionResult Index()
         {
-            return View();
+            Question question = questionService.getQuestion(1);
+            Language parent = languageService.getLanguage(1);
+            Language learn = languageService.getLanguage(2);
+            TestQuestionViewModel viewModel = new TestQuestionViewModel() { Question = question, ParentLanguage = parent, LearnLanguage = learn };
+
+            string content = viewModel.getQuestionText();
+
+            return View(viewModel);
+
         }
 
     }
---
+++ Typer.Web/Models/TestQuestionViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Typer.Domain.Entities;
+
+namespace Typer.Web.Models
+{
+    public class TestQuestionViewModel
+    {
+        public Question Question { get; set; }
+        public Language ParentLanguage { get; set; }
+        public Language LearnLanguage { get; set; }
+        private QuestionOption selectedOption;
+
+        public string getQuestionText()
+        {
+            IEnumerable<QuestionOption> options = Question.getOptions(ParentLanguage.Id);
+            selectedOption = selectOption(options);
+            return (selectedOption != null ? selectedOption.Content : "");
+        }
+
+
+        private QuestionOption selectOption(IEnumerable<QuestionOption> options)
+        {
+            if (options.Count() == 1)
+            {
+                return options.Single();
+            }
+            else
+            {
+                int weights = options.Sum(q => q.Weight);
+                int randomWeight = new Random().Next(1, weights + 1);
+                int current = 0;
+
+                foreach (QuestionOption option in options)
+                {
+                    current += option.Weight;
+                    if (randomWeight <= current)
+                    {
+                        return option;
+                    }
+                }
+
+                return null;
+
+            }
+        }
+
+    }
+}
