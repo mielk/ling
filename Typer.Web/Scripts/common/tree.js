04a57c993823718aee68444ced37e1425e012570
mielk <mielk@o2.pl>
2013-11-18 17:15:37 +0100

    JS Tree View - expanding by shortcuts and navigation

--- Typer.Web/Scripts/common/tree.js
+++ Typer.Web/Scripts/common/tree.js
@@ -136,8 +136,6 @@ function TreeView(container, selectable) {
     this.events = jQuery('<div/>', {
         'class': 'events-container'
     }).appendTo(this.container);
-    this.transfer = new NodesTransferManager(this, this.events);
-
 
     this.getContainer = function () {
         return this.container;
@@ -146,62 +144,119 @@ function TreeView(container, selectable) {
     this.root = new TreeNode(me, 'root', 'root', me, true);
     this.root.loadData(data);
 
-}
-TreeView.prototype.trigger = function (e) {
-    this.events.trigger(e);
-}
+
+    this.dragDropManager = (function () {
+        var drag = null;
+        var drop = null;
+
+        $(document).bind({
+            'mousemove': function (e) {
+                e.preventDefault();
+                if (e && drag) {
+
+                    var x = e.pageX;
+                    var y = e.pageY;
+
+                    if (drop) {
+                        var previousArea = drop.dropArea;
+                        if (previousArea.isHovered(x, y)) {
+                            return;
+                        } else {
+                            previousArea.unselect();
+                        }
+                    } else {
+                        me.root.dropArea.check(x, y);
+                    }
+                }
+            }
+        });
+
+        $(me.events).bind({
+            dropin: function (e) {
+                if (e.node !== drag) {
+                    drop = e.node;
+                    $('#drop').html('Drop: ' + drop.getName());
+                }
+            },
+            dropout: function (e) {
+                if (drop === e.node) {
+                    drop = null;
+                    $('#drop').html('Drop: ' + 'null');
+                }
+            },
+            dragin: function (e) {
+                drag = e.node;
+                $('#drag').html('Drag: ' + drag.getName());
+            },
+            dragout: function (e) {
+                $('#drag').html('Drag: ' + 'null');
+                if (drag && drop) {
+                    me.trigger({
+                        type: 'transfer',
+                        node: drag,
+                        to: drop
+                    });
+                    drag = null;
+                }
+            },
+            transfer: function (e) {
+                if (e.node && e.to) {
+                    e.node.transfer(e.to);
+                }
+            }
+        });
 
 
-function NodesTransferManager(tree, listener) {
-    var drag = null;
-    var drop = null;
+        return {
+            hasDropArea: function () {
+                return (drop ? true : false);
+            },
+            getDrag: function () {
+                return drag;
+            }
+        }
 
-    $(listener).bind({
-        dropin: function (e) {
-            drop = e.node;
-            //$('#drop').html('Drop: ' + drop.getName());
+    })();
+
+    this.activeNode = null;
+
+    this.events.bind({
+        expand: function (e) {
+            me.root.dropArea.recalculate();
         },
-        dropout: function (e) {
-            if (drop === e.node) {
-                drop = null;
-                //$('#drop').html('Drop: ' + 'null');
+        collapse: function (e) {
+            me.root.dropArea.recalculate();
+        },
+        activate: function (e) {
+            if (me.activeNode) {
+                me.activeNode.inactivate();
             }
-        },
-        dragin: function (e) {
-            drag = e.node;
-            //$('#drag').html('Drag: ' + drag.getName());
-        },
-        dragout: function (e) {
-            //$('#drag').html('Drag: ' + 'null');
-            if (drag && drop) {
-                tree.trigger({
-                    type: 'transfer',
-                    node: drag,
-                    to: drop
-                });
-                drag = null;
-            }
-        },
-        transfer: function (e) {
-            if (e.node && e.to) {
-                e.node.transfer(e.to);
-            }
+            me.activeNode = e.node;
         }
     });
 
 }
 
+TreeView.prototype.trigger = function (e) {
+    this.events.trigger(e);
+}
+
+
+
 
 function TreeNode(tree, key, name, parent, expanded) {
     var me = this;
-    this.isNode = function () { return true; }
     this.tree = tree;
     this.key = key;
     this.name = name;
     this.parent = parent;
     this.nodes = {};
+    this.nodesArray = [];
+    this.index = 0;
     this.mouseClicked = false;
-    this.isSelected = false;
+    this.isActive = false;
+    this.isNode = true;
+    this.justEntered = false;   //Pozwala zapobiegać wielokrotnemu wywoływaniu.
 
     this.mainContainer = jQuery('<div/>', {
         id: key + '_container',
@@ -239,33 +294,50 @@ function TreeNode(tree, key, name, parent, expanded) {
             }).
             appendTo(me.line);
 
-        function expand () {
+        function _expand() {
+            if (!expandable) {
+                return;
+            }
+
             expanded = true;
             $(button).html('-');
             display(me.container, true);
+            me.tree.trigger({
+                type: 'expand',
+                node: me
+            });
         }
 
-        function collapse() {
+        function _collapse() {
+            if (!expandable) {
+                return;
+            }
+
             expanded = false;
             $(button).html('+');
             display(me.container, false);
+            me.tree.trigger({
+                type: 'collapse',
+                node: me
+            });
         }
 
         function _revertStatus() {
             if (expanded === true) {
-                collapse();
+                _collapse();
             } else if (expanded === false) {
-                expand();
+                _expand();
             }
         }
 
 
         function _setStatus(value) {
             if (value === true) {
-                expand();
+                _expand();
             } else if (value === false) {
-                collapse();
+                _collapse();
             }
+
         }
 
 
@@ -290,12 +362,109 @@ function TreeNode(tree, key, name, parent, expanded) {
             },
             setStatus: function (value) {
                 _setStatus(value);
+            },
+            expand: function(){
+                _expand();
+            },
+            collapse: function(){
+                _collapse();
+            },
+            isExpanded: function () {
+                return (expandable === false || expanded === false ? false : true);
             }
-
         }
 
     })(expanded);
 
+
+    this.navigator = (function () {
+
+        $(document).bind({
+            'keydown': function (e) {
+                if (me.isActive && !me.justEntered) {
+                    switch(e.which){
+                        case 37:
+                            _collapse(); 
+                            e.stopPropagation();
+                            break;
+                        case 38:
+                            e.stopPropagation();
+                            e.preventDefault();
+                            _moveUp();
+                            break;
+                        case 39:
+                            _expand();
+                            e.stopPropagation();
+                            break;
+                        case 40:
+                            e.stopPropagation();
+                            e.preventDefault();
+                            _moveDown();
+                            break;
+                    }
+                }
+
+                me.justEntered = false;
+
+            }
+
+        });
+
+
+        function _expand() {
+            if (me.expander) {
+                me.expander.expand();
+            }
+        }
+
+        function _collapse() {
+            if (me.expander) {
+                me.expander.collapse();
+            }
+        }
+
+        function _moveUp() {
+            var previousNode = me.previousNode();
+            if (previousNode) {
+                changeSelection(previousNode, false);
+            } else {
+                if (!me.isRoot()) {
+                    changeSelection(me.parent, false);
+                }
+            }
+        }
+
+        function _moveDown() {
+            if (me.expander && me.expander.isExpanded()) {
+                var childNode = me.getChildNode(0);
+                if (childNode) {
+                    changeSelection(childNode, true);
+                }
+            } else {
+                if (!me.isRoot()) {
+                    var nextNode = me.nextNode();
+
+                    if (nextNode === null) {
+                        nextNode = me.parent.nextNode();
+
+                    }
+
+                    if (nextNode) {
+                        changeSelection(nextNode, true);
+                    }
+
+                }
+            }
+        }
+        //Źle działa dla bjtaa.
+
+        function changeSelection(node, stop) {
+            me.inactivate();
+            node.activate();
+            node.justEntered = stop;
+        }
+
+    })();
 
 
     this.caption = jQuery('<div/>', {
@@ -306,32 +475,19 @@ function TreeNode(tree, key, name, parent, expanded) {
     bind({
         'mousedown': function (e) {
             e.preventDefault();
-
             me.mouseClicked = true;
-            if (me.isSelected) {
+            if (me.isActive) {
                 e.preventDefault;
                 me.renamer.activate();
-                me.unselect();
-                me.mouseClicked = false;
+                me.inactivate();
+                e.stopPropagation();
             }
         },
         'mouseup': function (e) {
             if (!me.mover.isActive() && me.mouseClicked) {
-                me.select();
+                me.activate();
             }
             me.mouseClicked = false;
-        },
-        'mouseover': function (e) {
-            me.tree.trigger({
-                type: 'dropin',
-                node: me
-            });
-        },
-        'mouseout': function (e) {
-            me.tree.trigger({
-                type: 'dropout',
-                node: me
-            });
         }
     }).
     appendTo(this.line);
@@ -340,6 +496,13 @@ function TreeNode(tree, key, name, parent, expanded) {
 
 
     $(document).bind({
+        'mousedown' : function(e){
+            if (me.renamer && me.renamer.isActive()) {
+                if (e && me.renamer.isOutside(e.pageX, e.pageY)) {
+                    me.renamer.escape();
+                }
+            }
+        },
         'mousemove': function (e) {
             e.preventDefault();
             if (me.mouseClicked) {
@@ -354,25 +517,114 @@ function TreeNode(tree, key, name, parent, expanded) {
     });
 
 
-    this.select = function () {
-        this.isSelected = true;
-        $(this.caption).addClass('selected');
-    };
-
-    this.unselect = function () {
-        this.isSelected = false;
-        $(this.caption).removeClass('selected');
-    };
-
     this.getContainer = function () {
         return this.container;
     };
+
 
     this.position = function (div) {
         return $(div).offset();
     }
 
+    this.dropArea = (function () {
+        var visible = false;
+        var selected = false;
+        var xa = 0;
+        var xz = 0;
+        var ya = 0;
+        var yz = 0;
 
+        function _calculatePosition() {
+            var caption = me.caption;
+            var position = $(caption).offset();
+            var width = $(caption).width();
+            var height = $(caption).height();
+
+            if (width === 0 || height === 0) {
+                visible = false;
+            } else {
+                visible = true;
+                xa = position.left;
+                ya = position.top;
+                xz = xa + width;
+                yz = ya + height;
+            }
+        };
+
+        function _isHovered(x, y) {
+            if (visible) {
+                if (x >= xa && x <= xz && y >= ya && y <= yz) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        function _check(x, y) {
+            if (me.tree.dragDropManager.hasDropArea()) {
+                return;
+            } else if (_isHovered(x, y) && me.tree.dragDropManager.getDrag() !== me) {
+                _select();
+            } else {
+                for (var key in me.nodes) {
+                    if (me.nodes.hasOwnProperty(key)) {
+                        var node = me.nodes[key];
+                        if (node) {
+                            node.dropArea.check(x, y);
+                        }
+                    }
+                }
+            }
+        }
+
+        function _unselect() {
+            //Switch off as drop area.
+            $(me.caption).removeClass('drop-area');
+            me.tree.trigger({
+                type: 'dropout',
+                node: me
+            });
+        }
+
+        function _select() {
+            $(me.caption).addClass('drop-area');
+            me.tree.trigger({
+                type: 'dropin',
+                node: me
+            });
+        }
+
+        //Setting initial position.
+        _calculatePosition();
+
+
+        return {
+            calculatePosition: function () {
+                _calculatePosition();
+            },
+            unselect: function () {
+                _unselect();
+            },
+            isHovered: function(x, y){
+                return _isHovered(x, y);
+            },
+            check: function (x, y) {
+                _check(x, y);
+            },
+            recalculate: function () {
+                _calculatePosition();
+                for (var key in me.nodes) {
+                    if (me.nodes.hasOwnProperty(key)) {
+                        var node = me.nodes[key];
+                        if (node) {
+                            node.dropArea.recalculate();
+                        }
+                    }
+                }
+            }
+        }
+
+    })();
 
     this.mover = (function () {
         var active = false;
@@ -492,7 +744,6 @@ function TreeNode(tree, key, name, parent, expanded) {
             return control;
         }
 
-
         function validateName(name) {
             return true;
         }
@@ -512,7 +763,6 @@ function TreeNode(tree, key, name, parent, expanded) {
             $(textbox).remove();
             textbox = null;
         }
-
 
         function _activate() {
             if (textbox === null) {
@@ -542,34 +792,63 @@ function TreeNode(tree, key, name, parent, expanded) {
             },
             escape: function () {
                 _escape();
+            },
+            isOutside: function (x, y) {
+                var position = $(textbox).offset();
+                var xa = position.left;
+                var ya = position.top;
+                var xz = xa + $(textbox).width();
+                var yz = ya + $(textbox).height();
+
+                if (x >= xa && x <= xz && y >= ya && y <= yz) {
+                    return false;
+                }
+
+                return true;
+
             }
         }
 
     })();
 
+    this.sorter = (function () {
+        me.nodesArray = []
 
-    this.sortNodes = function () {
-        if (Object.keys(this.nodes).length > 1) {
-
-            var nodes = [];
-            for (var key in this.nodes) {
-                if (this.nodes.hasOwnProperty(key)) {
-                    nodes.push(this.nodes[key]);
+        var createNodesTable = function () {
+            for (var key in me.nodes) {
+                var index = 0;
+                if (me.nodes.hasOwnProperty(key)) {
+                    var node = me.nodes[key];
+                    me.nodesArray.push(node);
                 }
             }
-            nodes.sort(function (obj1, obj2) {
+        }
+
+        var sort = function () {
+            me.nodesArray.sort(function (obj1, obj2) {
                 return (obj1.name < obj2.name ? -1 : 1);
             });
-
-            for (var i = nodes.length - 1; i > 0; i--) {
-                $(nodes[i].mainContainer).before($(nodes[i-1].mainContainer));
-                //$(this.nodes[i].mainContainer).before($(this.nodes[i - 1].mainContainer));
-            }
-
-        } else {
-            alert('not sort');
         }
-    };
+
+        var rearrange = function () {
+            for (var i = me.nodesArray.length - 1; i > 0; i--) {
+                $(me.nodesArray[i].mainContainer).before($(me.nodesArray[i - 1].mainContainer));
+                me.nodesArray[i].index = i;
+            }
+        }
+
+        return {
+            sort: function () {
+                me.nodesArray = [];
+                createNodesTable();
+                if (Object.keys(me.nodes).length > 1) {
+                    sort();
+                    rearrange();
+                }
+            }
+        }
+
+    })();
 
 }
 
@@ -584,12 +863,13 @@ TreeNode.prototype.loadData = function (data) {
                 var item = data[key];
                 var node = new TreeNode(this.tree, item.key, item.caption, this, item.expanded);
                 node.loadData(item.items);
-                this.nodes[key] = node;
+                this.nodes[item.key] = node;
             }
         }
 
-    }
+        this.sorter.sort();
 
+    }
 }
 TreeNode.prototype.getName = function () {
     return this.name;
@@ -599,7 +879,7 @@ TreeNode.prototype.getKey = function () {
 }
 TreeNode.prototype.transfer = function (destination) {
     if (this === destination) {
-        this.isSelected = false;
+        this.isActive = false;
     } else {
         if (!this.isRoot()) {
             this.parent.removeNode(this);
@@ -611,19 +891,23 @@ TreeNode.prototype.removeNode = function (node) {
     delete this.nodes[node.getKey()];
 }
 TreeNode.prototype.addNode = function (node) {
+    //alert('moving node ' + node.name + ' to ' + this.name);
     this.nodes[node.getKey()] = node;
     node.moveTo(this);
-    this.sortNodes();
+    this.sorter.sort();
     this.resetStatus();
+    this.dropArea.unselect();
+    this.dropArea.recalculate();
 }
 TreeNode.prototype.moveTo = function (newParent) {
+    if (this.parent)
+        delete this.parent[this.key];
+    this.parent = newParent;
     this.mainContainer.appendTo($(newParent.getContainer()));
 }
 TreeNode.prototype.isRoot = function () {
-    return (this.parent.hasOwnProperty('isNode') ? false : true);
-}
-TreeNode.prototype.isNode = function () {
-    return true;
+    var value = (this.parent.isNode ? false : true);
+    return value;
 }
 TreeNode.prototype.resetStatus = function () {
     if (Object.keys(this.nodes).length > 0) {
@@ -632,6 +916,39 @@ TreeNode.prototype.resetStatus = function () {
         this.expander.setExpandableStatus(false);
     }
 }
+TreeNode.prototype.getChildNode = function (i) {
+    if (i >= 0 && i < this.nodesArray.length) {
+        return this.nodesArray[i];
+    }
+    return null;
+}
+TreeNode.prototype.activate = function () {
+    this.isActive = true;
+    $(this.caption).addClass('selected');
+    this.tree.trigger({
+        'type': 'activate',
+        'node': this
+    });
+}
+TreeNode.prototype.inactivate = function () {
+    this.isActive = false;
+    $(this.caption).removeClass('selected');
+}
+TreeNode.prototype.nextNode = function () {
+    if (this.isRoot() === true) {
+        return null;
+    } else {
+        return this.parent.getChildNode(this.index + 1);
+    }
+}
+TreeNode.prototype.previousNode = function () {
+    if (this.isRoot() === true) {
+        return null;
+    } else {
+        return this.parent.getChildNode(this.index - 1);
+    }
+}
+
 
 
 function hide(div) {
