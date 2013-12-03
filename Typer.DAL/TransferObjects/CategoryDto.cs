95aeb8e20b35d41bf77a5bae1b64285762f8a4c8
mielk <mielk@o2.pl>
2013-12-03 10:12:04 +0100

    Classes Category and CategoryDto added.

---
+++ Typer.DAL/TransferObjects/CategoryDto.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Typer.DAL.TransferObjects
+{
+    public class CategoryDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ParentId { get; set; }
+    }
+}
---
+++ Typer.Domain/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Typer.Domain.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ParentId { get; set; }
+        public Category? Parent { get; set; }
+        public IEnumerable<Category> children { get; set; }
+    }
+}
