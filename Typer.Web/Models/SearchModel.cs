192f89110ff9b10583c2dbdeea2c0b1d332531ba
mielk <mielk@o2.pl>
2013-12-17 11:10:51 +0100

    Layout for search panel designed.

---
+++ Typer.Web/Models/SearchModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Typer.Web.Models
+{
+    public class SearchModel
+    {
+        public int WordType { get; set; }
+        public int LWeight { get; set; }
+        public int UWweight { get; set; }
+        public string Container { get; set; }
+    }
+}
